c37cfb310a36214a53085992fc114bf9d339f891
Julian Toscani <julian.toscani@rtl.com>
2022-08-02 23:04:27 +0200

    add script to create css variables from js Object

---
+++ src/buildVariablesCss.mjs
@@ -0,0 +1,47 @@
+import fs from "fs/promises";
+import colors from "./variables/colors.mjs";
+import typography from "./variables/typography.mjs";
+
+buildVariablesCss();
+
+function buildVariablesCss() {
+  const styleMap = createStyleMap(colors, typography);
+  const content = styleMapToString(styleMap);
+  fs.writeFile("./src/variables/styles.css", content);
+}
+
+function createStyleMap(...objects) {
+    const styleMap = new Map();
+    objects.forEach((object) => {
+        Object.entries(object.data).forEach(([key, value]) => {
+          propertyToCssVariable([key, value], `--${object.name}`, styleMap);
+        });
+    });
+
+    return styleMap;
+}
+
+function styleMapToString(styleMap) {
+    let variablesContent = ":root {\n";
+    
+    for (const [key, value] of styleMap) {
+      variablesContent += `   ${key}: ${value};\n`;
+    }
+    
+    variablesContent += "}";
+    return variablesContent;
+}
+
+function propertyToCssVariable([key, value], prefix = "-", map) {
+  switch (typeof value) {
+    case "object":
+      return Object.entries(value).map((entry) =>
+        propertyToCssVariable(entry, prefix + `-${key}`, map)
+      );
+    case "string":
+      map.set(`${prefix}-${key}`, value);
+      break;
+    default:
+      throw new Error(`Unsupported property type '${typeof value}'`);
+  }
+}
