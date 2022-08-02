fc6349f1c3b6878fd4e4c596880d6be1814494cc
Julian Toscani <julian.toscani@rtl.com>
2022-08-02 23:05:08 +0200

    addd colros

---
+++ src/variables/colors.mjs
@@ -0,0 +1,10 @@
+const data = {
+    "primary": {
+        "100": "lightred",
+        "200": "red",
+        "300": "darkred",
+        "400": "darkestred"
+    }
+}
+
+export default {name: "colors", data};
