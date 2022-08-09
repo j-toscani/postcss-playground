5a89c9700c27455ac986c5fa91c185901db36a4c
Julian Toscani <julian.toscani@rtl.com>
2022-08-09 21:32:27 +0200

    create typescale

--- src/variables/typography.mjs
+++ src/variables/typography.mjs
@@ -1,5 +1,8 @@
 import createTypeScale from "../utils/createTypeScale.mjs";
 
-const data = createTypeScale();
+const data = createTypeScale({
+  base: 0.875,
+  sizes: Array.from(new Array(8), (e, i) => (i + 1).toString()),
+});
 
-export default { name: "text", data}
+export default { name: "text", data };
