b932b4fac7e6a6c47211631b1ca335c4898f43de
Julian Toscani <julian.toscani@rtl.com>
2022-08-10 08:36:05 +0200

    update typography and zindex

--- src/variables/typography.mjs
+++
@@ -1,8 +0,0 @@
-import createTypeScale from "../utils/createTypeScale.mjs";
-
-const data = createTypeScale({
-  base: 0.875,
-  sizes: Array.from(new Array(8), (e, i) => (i + 1).toString()),
-});
-
-export default { name: "text", data };
