46136af6758d962ed384a4ac8372361b665cc3b4
Julian Toscani <julian.toscani@rtl.com>
2022-08-10 08:40:43 +0200

    refactor variable creation

--- src/buildVariablesCss.mjs
+++ src/buildVariablesCss.mjs
@@ -3,21 +3,22 @@ import colors from "./variables/colors.mjs";
 import shadows from "./variables/shadows.mjs";
 import radius from "./variables/radius.mjs";
 import spacing from "./variables/spacing.mjs";
-import typography from "./variables/typography.mjs";
+import fonts from "./variables/fonts.mjs";
+import zindex from "./variables/zindex.mjs";
 
 buildVariablesCss();
 
 function buildVariablesCss() {
-  const styleMap = createStyleMap(colors, typography, spacing, radius, shadows);
+  const styleMap = createStyleMap([colors, fonts, spacing, radius, shadows, zindex], 'ck');
   const content = styleMapToString(styleMap);
   fs.writeFile("./src/variables/output.css", content);
 }
 
-function createStyleMap(...objects) {
+function createStyleMap(objects, prefix) {
     const styleMap = new Map();
     objects.forEach((object) => {
         Object.entries(object.data).forEach(([key, value]) => {
-          propertyToCssVariable([key, value], `--${object.name}`, styleMap);
+          propertyToCssVariable([key, value], `--${prefix}-${object.name}` ?? `--${object.name}`, styleMap);
         });
     });
 
@@ -41,6 +42,8 @@ function propertyToCssVariable([key, value], prefix = "-", map) {
       return Object.entries(value).map((entry) =>
         propertyToCssVariable(entry, prefix + `-${key}`, map)
       );
+    case "number":
+      value = value.toString();
     case "string":
       map.set(`${prefix}-${key}`, value);
       break;
